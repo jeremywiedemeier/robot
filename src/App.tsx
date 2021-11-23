70459624655f70f6bcebace91e82c7a120f8d0bc
Jeremy W <jeremywiedemeier@gmail.com>
2021-11-23 11:32:23 -0500

    basic movement control - client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import Controller from "./components/Controller";
-import RobotAnimation from "./components/RobotAnimation";
+// import RobotAnimation from "./components/RobotAnimation";
 
 const App: React.FC = () => {
   return (
--- src/components/Controller/index.tsx
+++ src/components/Controller/index.tsx
@@ -1,37 +1,90 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const handleKeyDown =
-  (setActiveKeys: React.Dispatch<React.SetStateAction<string[]>>) =>
+  (
+    setActiveKeys: React.Dispatch<React.SetStateAction<string[]>>,
+    socket: WebSocket | null
+  ) =>
   (evt: KeyboardEvent) => {
     if (["w", "a", "s", "d"].includes(evt.key))
       setActiveKeys((currentActiveKeys) => {
+        if (currentActiveKeys.includes(evt.key)) {
+          return currentActiveKeys;
+        }
         const newActiveKeys = [...currentActiveKeys, evt.key];
+        if (socket) {
+          console.log(
+            `keydown ${evt.key}, sending active keys ${newActiveKeys.join()}`
+          );
+          socket.send(newActiveKeys.join());
+        }
         return newActiveKeys;
       });
   };
 
 const handleKeyUp =
-  (setActiveKeys: React.Dispatch<React.SetStateAction<string[]>>) =>
+  (
+    setActiveKeys: React.Dispatch<React.SetStateAction<string[]>>,
+    socket: WebSocket | null
+  ) =>
   (evt: KeyboardEvent) => {
     if (["w", "a", "s", "d"].includes(evt.key))
       setActiveKeys((currentActiveKeys) => {
         const newActiveKeys = currentActiveKeys.filter(
           (key) => key !== evt.key
         );
+        if (socket) {
+          console.log(
+            `keyup ${evt.key}, sending active keys ${newActiveKeys.join()}`
+          );
+          socket.send(newActiveKeys.join());
+        }
         return newActiveKeys;
       });
   };
 
 const Controller: React.FC = () => {
+  const socket = useRef<WebSocket | null>(null);
+
+  useEffect(() => {
+    console.log("opening websocket connection");
+    socket.current = new WebSocket("ws://nastberry:8765");
+    socket.current.onopen = () => {
+      console.log("websocket opened");
+    };
+    socket.current.onmessage = (evt) => {
+      console.log(evt.data);
+    };
+    socket.current.onclose = () => {
+      console.log("websocket closed");
+    };
+
+    return () => {
+      socket.current?.close();
+    };
+  }, []);
   const [activeKeys, setActiveKeys] = useState<string[]>([]);
 
   useEffect(() => {
-    document.addEventListener("keydown", handleKeyDown(setActiveKeys));
-    document.addEventListener("keyup", handleKeyUp(setActiveKeys));
+    console.log("adding event listeners");
+    document.addEventListener(
+      "keydown",
+      handleKeyDown(setActiveKeys, socket.current)
+    );
+    document.addEventListener(
+      "keyup",
+      handleKeyUp(setActiveKeys, socket.current)
+    );
 
     return () => {
-      document.removeEventListener("keydown", handleKeyDown(setActiveKeys));
-      document.removeEventListener("keyup", handleKeyUp(setActiveKeys));
+      document.removeEventListener(
+        "keydown",
+        handleKeyDown(setActiveKeys, socket.current)
+      );
+      document.removeEventListener(
+        "keyup",
+        handleKeyUp(setActiveKeys, socket.current)
+      );
     };
   }, []);
 
